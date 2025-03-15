420e62134a8d8fc3ef2b97f834542e39c429c52f
kidex <kyrylderenko@gmail.com>
2025-03-15 11:36:05 +0200

    first commit

--- git.py
+++ git.py
@@ -1,2 +1,18 @@
-print("Hello!")
-print("Goodbye")
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.grades = []
+
+    def add_grade(self, grade):
+        self.grades.append(grade)
+
+    def get_avg(self):
+        print
+
+
+obj = Student("Кирил", 14)
+obj.add_grade(9)
+obj.add_grade(12)
+obj.add_grade(10)
+
