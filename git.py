a5567cb3f9dae71a6d8550de22b8a06ee52a2250
kidex <kyrylderenko@gmail.com>
2025-03-21 20:15:47 +0200

    first commit

--- git.py
+++ git.py
@@ -1,18 +1,18 @@
-class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-        self.grades = []
 
-    def add_grade(self, grade):
-        self.grades.append(grade)
+class Counter:
+    def __init__(self):
+        self.count = 0
+    def increment(self):
+        self.count += 1
+    def decrement(self):
+        self.count -= 1
+    def reset(self):
+        self.count = 0
 
-    def get_avg(self):
-        print
+obj = Counter()
+obj.increment()
+obj.increment()
+obj.decrement()
+obj.reset()
 
-
-obj = Student("Кирил", 14)
-obj.add_grade(9)
-obj.add_grade(12)
-obj.add_grade(10)
 
